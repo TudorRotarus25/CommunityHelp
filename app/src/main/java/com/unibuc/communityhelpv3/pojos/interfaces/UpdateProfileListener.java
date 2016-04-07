5a08860f52cf03c777e783759bab7b23889641bd
tudor.rotarus <tudor.rotarus@gmail.com>
2016-04-07 16:24:14 +0300

    Implemented all methods in NetworkManager: create task, get my tasks, get profile, update profile

---
+++ app/src/main/java/com/unibuc/communityhelpv3/pojos/interfaces/UpdateProfileListener.java
@@ -0,0 +1,9 @@
+package com.unibuc.communityhelpv3.pojos.interfaces;
+
+/**
+ * Created by Tudor on 07.04.2016.
+ */
+public interface UpdateProfileListener {
+    void onUpdateProfileSuccess();
+    void onUpdateProfileFailed();
+}
--- app/src/main/java/com/unibuc/communityhelpv3/rest/RestAPI.java
+++ app/src/main/java/com/unibuc/communityhelpv3/rest/RestAPI.java
@@ -2,12 +2,15 @@ package com.unibuc.communityhelpv3.rest;
 
 import com.unibuc.communityhelpv3.pojos.CategoriesGetBody;
 import com.unibuc.communityhelpv3.pojos.LoginPostBody;
+import com.unibuc.communityhelpv3.pojos.TasksGetBody;
+import com.unibuc.communityhelpv3.pojos.UserGetBody;
 
 import retrofit.Call;
 import retrofit.http.Field;
 import retrofit.http.FormUrlEncoded;
 import retrofit.http.GET;
 import retrofit.http.POST;
+import retrofit.http.Path;
 
 /**
  * Created by Tudor on 04.03.2016.
@@ -18,6 +21,21 @@ public interface RestAPI {
     @POST("users/login")
     Call<LoginPostBody> LOGIN_POST_BODY_CALL(@Field("first_name") String firstName, @Field("last_name") String lastName, @Field("facebook_token") String facebookToken, @Field("profile_pic") String profilePicUri);
 
+    @GET("users/profile/{user_id}")
+    Call<UserGetBody> USER_GET_BODY_CALL(@Path("user_id") String id);
+
+    @FormUrlEncoded
+    @POST("users/profile")
+    Call<Void> USER_POST_UPDATE_CALL(@Field("facebook_token") String facebookToken, @Field("first_name") String firstName, @Field("last_name") String lastName, @Field("profile_pic") String profilePic);
+
     @GET("labels/categories")
-    Call<CategoriesGetBody> CATEORIES_GET_BODY_CALL();
- }
+    Call<CategoriesGetBody> CATEGORIES_GET_BODY_CALL();
+
+    @FormUrlEncoded
+    @POST("tasks/create")
+    Call<Void> TASK_POST_CREATE_CALL(@Field("facebook_token") String facebookToken, @Field("title") String title, @Field("description") String description, @Field("category_id") int categoryId, @Field("resource_cost") int resourceCost, @Field("time_cost") int timeCost);
+
+    @FormUrlEncoded
+    @POST("tasks/my_tasks")
+    Call<TasksGetBody> MY_TASKS_GET_BODY_CALL(@Field("facebook_token") String facebookToken);
+}
